6687c0306026a8fab4e187412baf5e744f4e9170
Komal Mohil <komalmohil1@gmail.com>
2025-08-12 16:36:50 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'    ///default 
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App2.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h5>From main.jsx</h5>
+    <App/>
+  </StrictMode>,
+)
+
+
+  // or like CRA
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css'
+// import App from './App.jsx'
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <h5>From index.js</h5>
+//     <App />
+//   </React.StrictMode>
+// );
